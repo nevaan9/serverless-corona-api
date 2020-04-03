6ce3944cbefeef32b79a0a309106598414a8d1c8
Nevaan Perera <nevaan9@gmail.com>
2020-04-03 08:32:39 -0500

    got different structure in place

--- get.py
+++ get.py
@@ -3,6 +3,16 @@ import boto3
 import datetime
 import os
 import json
+import uuid
+
+'''
+Items: [{
+    'active_cases': {
+        'S': '74'
+    },
+    ...
+},...]
+'''
 
 def getNumericalValues(item, propName, firstPrecedence, secondPrecedence):
     data = None
@@ -22,6 +32,19 @@ def normalize(item):
         'deaths': getNumericalValues(item, 'deaths', 'N', 'S'),
         'new_deaths': getNumericalValues(item, 'new_deaths', 'N', 'S'),
         'serious_critical': getNumericalValues(item, 'serious_critical', 'N', 'S'),
+    }
+
+def veoci_normalize_entries(item):
+    return {
+        'custom_0': getNumericalValues(item, 'country', 'S', 'S'),
+        'custom_1': getNumericalValues(item, 'created_at', 'S', 'S'),
+        'custom_2': getNumericalValues(item, 'cases', 'N', 'S'),
+        'custom_3': getNumericalValues(item, 'active_cases', 'N', 'S'),
+        'custom_4': getNumericalValues(item, 'recovered', 'N', 'S'),
+        'custom_5': getNumericalValues(item, 'deaths', 'N', 'S'),
+        'custom_6': getNumericalValues(item, 'new_deaths', 'N', 'S'),
+        'custom_7': getNumericalValues(item, 'serious_critical', 'N', 'S'),
+        'id': str(uuid.uuid4())
     }
 
 def main(event, context):
@@ -48,7 +71,63 @@ def main(event, context):
                         ExpressionAttributeValues={ ":created_at": { "S": str(yesterday) } },
                     )
             data = r['Items']
-        return_data = list(map(normalize, data))
+        entries = list(map(veoci_normalize_entries, data))
+        fields = []
+        if len(entries) > 0:
+            fields = [
+                {
+                    'name': 'country',
+                    'type': 'TEXT',
+                    'fieldId': str(uuid.uuid4()),
+                    'index': 0
+                },
+                {
+                    'name': 'created_at',
+                    'type': 'DATE',
+                    'fieldId': str(uuid.uuid4()),
+                    'index': 1
+                },
+                {
+                    'name': 'cases',
+                    'type': 'NUMERIC',
+                    'fieldId': str(uuid.uuid4()),
+                    'index': 2
+                },
+                {
+                    'name': 'active_cases',
+                    'type': 'NUMERIC',
+                    'fieldId': str(uuid.uuid4()),
+                    'index': 3
+                },
+                {
+                    'name': 'recovered',
+                    'type': 'NUMERIC',
+                    'fieldId': str(uuid.uuid4()),
+                    'index': 4
+                },
+                {
+                    'name': 'deaths',
+                    'type': 'TEXT',
+                    'fieldId': str(uuid.uuid4()),
+                    'index': 5
+                },
+                {
+                    'name': 'new_deaths',
+                    'type': 'TEXT',
+                    'fieldId': str(uuid.uuid4()),
+                    'index': 6
+                },
+                {
+                    'name': 'serious_critical',
+                    'type': 'NUMERIC',
+                    'fieldId': str(uuid.uuid4()),
+                    'index': 7
+                }
+            ]
+        return_data = {
+            'entries': entries,
+            'fields': fields
+        }
         return {
             'statusCode': 200,
             'body': json.dumps(return_data)
